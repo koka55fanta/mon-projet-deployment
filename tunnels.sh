c2e70422f7859f1aa9698af838f895e1e7bf0b04
koka_fanta <abdo44jerray@gmail.com>
2026-05-14 18:55:22 +0000

    fix: ajout de l'attente pour Grafana dans le script de démarrage

--- tunnels.sh
+++ tunnels.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 echo "🔄 1. Attente du réveil de Kubernetes (K3s)..."
-# Cette boucle tourne dans le vide tant que l'API de K3s ne répond pas
 until kubectl get nodes > /dev/null 2>&1; do sleep 2; done
 
-echo "🚀 2. Réveil de l'infrastructure (ArgoCD & Monitoring)..."
+echo "🚀 2. Réveil de l'infrastructure..."
 kubectl scale deployment argocd-server argocd-repo-server argocd-dex-server argocd-notifications-controller -n argocd --replicas=1
 kubectl scale statefulset argocd-application-controller -n argocd --replicas=1
 kubectl scale deployment kube-stack-kube-prometheus-operator kube-stack-grafana -n monitoring --replicas=1
 kubectl scale statefulset prometheus-kube-stack-kube-prometheus-prometheus alertmanager-kube-stack-kube-prometheus-alertmanager -n monitoring --replicas=1
 
-echo "⏳ 3. Attente du démarrage complet (Patience, environ 1 à 2 minutes)..."
-# On force le script à bloquer ici tant qu'ArgoCD n'est pas prêt à 100%
+echo "⏳ 3. Attente de la disponibilité des outils DevOps..."
+# On attend ArgoCD
 kubectl wait --for=condition=available --timeout=120s deployment/argocd-server -n argocd
+# 🚀 NOUVEAU : On attend Grafana
+kubectl wait --for=condition=available --timeout=120s deployment/kube-stack-grafana -n monitoring
 
-echo "🌐 4. Lancement sécurisé des tunnels..."
+echo "🌍 4. Attente de l'application Web et MySQL (Patience...)"
+kubectl wait --for=condition=available --timeout=180s deployment/web
+
+echo "🌐 5. Lancement sécurisé des tunnels..."
 pkill -f "port-forward"
 sleep 2
 
